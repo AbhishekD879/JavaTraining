a64f090013f41a7c1024d69c73bc06a36cc367dd
AbhishekD879 <abhishekdiwate879@gmail.com>
2022-09-13 13:03:32 +0530

    leap

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    static boolean isLeapYear(int year) {
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Year:");
         int year = sc.nextInt();
-        System.out.println(isLeapYear(year)?"it is leap Year":"It's not leap year");
+        System.out.println(new Day1().isLeapYear(year)?"it is leap Year":"It's not leap year");
     }
 }
