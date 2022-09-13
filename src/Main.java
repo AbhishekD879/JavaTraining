f162b18bea4070b3b7b0fad01db0528447194411
AbhishekD879 <abhishekdiwate879@gmail.com>
2022-09-13 12:51:25 +0530

    Leap Year

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+    static boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
     public static void main(String[] args) {
-        System.out.println("Abhishek");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(isLeapYear(year)?"it is leap Year":"It's not leap year");
     }
 }
